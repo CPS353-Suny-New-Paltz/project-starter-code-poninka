4a565ab70a9da593c8fbbd276de8f911061e99d3
poninka <43103397+poninka@users.noreply.github.com>
2025-10-01 01:06:56 -0400

    Delete src/project/emptyimplementations directory

--- src/project/emptyimplementations/UserComputeAPIImplementation.java
+++
@@ -1,9 +0,0 @@
-package project.emptyimplementations;
-
-public class UserComputeAPIImplementation implements UserComputeAPI {
-    @Override
-    public UserSubResponse submit(UserSubmission submission) {
-        // Default failure until implemented
-        return new UserSubResponse(null, SubmissionStatus.FAILURE_SYSTEM_ERROR);
-    }
-}
