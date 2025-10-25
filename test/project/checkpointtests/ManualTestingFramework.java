98fc8b8d22a2773c67c498b8a2353e274ca8654b
poninka <Danielbagroff@gmail.com>
2025-10-25 02:00:03 -0400

    Update ManualTestingFramework.java

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -20,16 +20,16 @@ public class ManualTestingFramework {
         DataStoreComputeAPI dataStore = new DataStoreImplementation();
         ComputeControllerAPI computeEngine = new PowerDigitSumController();
 
-        // Load newline-separated integers
+        // Load newline integers
         List<Integer> inputs = dataStore.loadInput(INPUT, "\n");
 
-        // Compute one result per input (n^n digit sum)
+        // Compute one result per input
         List<String> results = inputs.stream()
                 .map(n -> computeEngine.compute(new ComputeRequest(n)))
                 .map(ComputeResponse::getResult)
                 .collect(Collectors.toList());
 
-        // Write a single comma-separated line to output
+        // Write a single line to output
         String outputLine = String.join(",", results);
         dataStore.saveOutput(OUTPUT, outputLine);
     }
