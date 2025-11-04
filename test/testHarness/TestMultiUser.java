d31a2fd3b9ecc8e9a7f494c4e497f67e7591219c
poninka <Danielbagroff@gmail.com>
2025-11-04 17:31:02 -0500

    Added test harness and multiple user test
    added TestMiltiUser and TestUser completed
    
    TODO fixes 1 - 4
    1 - UserComputeAPI testing interface
    2 - initializations
    3 - UserComputeAPI type
    4 - wrapper, output results
    
    added input file for TestMiltiUser

---
+++ test/testHarness/TestMultiUser.java
@@ -0,0 +1,86 @@
+package testHarness;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import project.conceptualapi.PowerDigitSumController;
+import project.networkapi.UserComputeAPI;
+import project.networkapi.UserComputeAPIImplementation;
+import project.processapi.DataStoreImplementation;
+
+public class TestMultiUser {
+
+    // 1: interface
+    private UserComputeAPI coordinator;
+
+    @BeforeEach
+    public void initializeComputeEngine() {
+        //2: create an instance of the implementation of @NetworkAPI
+        // Store it in the 'coordinator' variable
+        coordinator = new UserComputeAPIImplementation(new DataStoreImplementation(), new PowerDigitSumController());
+    }
+
+    @Test
+    public void compareMultiAndSingleThreaded() throws Exception {
+        int nThreads = 4;
+        List<testHarness.TestUser> testUsers = new ArrayList<>();
+        for (int i = 0; i < nThreads; i++) {
+            testUsers.add(new testHarness.TestUser(coordinator));
+        }
+
+        // Run single threaded
+        String singleThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.singleThreadOut.tmp";
+        for (int i = 0; i < nThreads; i++) {
+            File singleThreadedOut =
+                    new File(singleThreadFilePrefix + i);
+            singleThreadedOut.deleteOnExit();
+            testUsers.get(i).run(singleThreadedOut.getCanonicalPath());
+        }
+
+        // Run multi threaded
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        List<Future<?>> results = new ArrayList<>();
+        String multiThreadFilePrefix = "testMultiUser.compareMultiAndSingleThreaded.test.multiThreadOut.tmp";
+        for (int i = 0; i < nThreads; i++) {
+            File multiThreadedOut =
+                    new File(multiThreadFilePrefix + i);
+            multiThreadedOut.deleteOnExit();
+            String multiThreadOutputPath = multiThreadedOut.getCanonicalPath();
+            testHarness.TestUser testUser = testUsers.get(i);
+            results.add(threadPool.submit(() -> testUser.run(multiThreadOutputPath)));
+        }
+
+        results.forEach(future -> {
+            try {
+                future.get();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+
+        // Check that the output is the same for multi-threaded and single-threaded
+        List<String> singleThreaded = loadAllOutput(singleThreadFilePrefix, nThreads);
+        List<String> multiThreaded = loadAllOutput(multiThreadFilePrefix, nThreads);
+        Assertions.assertEquals(singleThreaded, multiThreaded);
+    }
+
+    private List<String> loadAllOutput(String prefix, int nThreads) throws IOException {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < nThreads; i++) {
+            File multiThreadedOut =
+                    new File(prefix + i);
+            result.addAll(Files.readAllLines(multiThreadedOut.toPath()));
+        }
+        return result;
+    }
+}
---
+++ test/testHarness/TestUser.java
@@ -0,0 +1,32 @@
+package testHarness;
+
+import java.io.File;
+import project.networkapi.Delimiter;
+import project.networkapi.InputSource;
+import project.networkapi.OutputSource;
+import project.networkapi.UserComputeAPI;
+import project.networkapi.UserSubmission;
+
+public class TestUser {
+
+    // 3: changed the type of this variable to the name used
+    // The parameter passed to the constructor
+    private final UserComputeAPI coordinator;
+
+    public TestUser(UserComputeAPI coordinator) {
+        this.coordinator = coordinator;
+    }
+
+    public void run(String outputPath) {
+        char delimiter = ';';
+        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+
+        // 4: Run the job through the coordinator with the input/output/delimiter
+        InputSource input = new InputSource("file", inputPath);
+        OutputSource output = new OutputSource(outputPath);
+        Delimiter delim = new Delimiter(delimiter);
+        UserSubmission submission = new UserSubmission(input, output, delim);
+        coordinator.submit(submission);
+    }
+
+}
